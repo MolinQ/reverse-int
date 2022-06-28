95e004c4c492bdd1306482648ebc23c270cf339f
kirill-5823 <kirillaidr@gmail.com>
2022-06-28 21:55:54 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    let a = -n;
+    a =a.toString().split("").reverse().join("");
+    return a;
+  }
+  else { 
+    n =n.toString().split("").reverse().join("");
+    return n;
+  }
 }
